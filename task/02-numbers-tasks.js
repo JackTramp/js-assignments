638418d94976ec0011e6cf9fe1312dcb7b82bd1d
JackTramp <jacktramp17@gmail.com>
2016-03-19 13:38:28 +0300

    Fix #8

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -182,8 +182,8 @@ function getParallelipidedDiagonal(a,b,c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-    return Math.round(num / Math.pow(10, pow)) * Math.pow(10, pow);
-    //throw new Error('Not implemented');
+    let n = Math.pow(10, pow);
+    return Math.round(num / n) * n;    
 }
 
 /**
