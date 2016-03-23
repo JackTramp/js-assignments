ff9afb4f5f2d9b00f350ad02e5730df9eb844473
JackTramp <jacktramp17@gmail.com>
2016-03-23 12:46:46 +0300

    Implement propagateItemsByPositionIndex

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -269,9 +269,8 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {  
-    throw new Error('Not implemented');
+    return arr.reduce((prev, curr, i) => prev.concat(new Array(i + 1).fill(curr)), []);    
 }
-
 
 /** 
  * Returns the 3 largest numbers from the specified array
